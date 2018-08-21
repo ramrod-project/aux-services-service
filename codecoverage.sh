4f442bd1f40479bb9b01dfd9c33fc23732464c58
themanzi <christophermmanzi@protonmail.com>
2018-08-21 15:51:34 -0500

    duh

--- codecoverage.sh
+++ codecoverage.sh
@@ -2,7 +2,7 @@
 
 ./cc-test-reporter before-build 
 for pkg in $(go list ./... | grep -v main); do
-    if [[ $pkg == "test" ]]; then
+    if [[ $pkg == "github.com/ramrod-project/aux-services-service/test" ]]; then
         continue
     fi
     go test -v -parallel 1 -coverprofile=$(echo $pkg | tr / -).cover $pkg
